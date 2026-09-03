19076c694c8a9fe9d578c7823ff528e1edec5350
dalilyagent-byte <dalily.agent@gmail.com>
2026-09-03 16:38:11 +0300

    Give Dalily web and Google execution tools
    * Add manager execution tools
    
    * Add manager execution tools
    
    * Add manager execution tools

--- api/google-connect.js
+++ api/google-connect.js
@@ -8,7 +8,8 @@ const SCOPES = [
   'https://www.googleapis.com/auth/gmail.send',
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/contacts',
-  'https://www.googleapis.com/auth/drive'
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/documents'
 ];
 
 export default async function handler(req, res) {
